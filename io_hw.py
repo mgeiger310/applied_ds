757a6b8b8855a38c2f5a333f37cd0f614debab44
Kushant Sunkara <kushantsunkara@Kushant-Sunkaras-MacBook-Pro-3.local>
2019-04-28 17:08:59 -0400

    file taking too long to load, but saving progress

--- io_hw.py
+++ io_hw.py
@@ -15,4 +15,7 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+    df = pd.read_csv('https://tufts.box.com/shared/static/eu8kpn2wetgpk0tgsnxtnbiv56adioem.csv')
+        head_df = df.head(5)
+        head_df.to_csv(out_path) 
     return df, head_df
