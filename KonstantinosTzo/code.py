3d5cf42c1360eb00280a67fc7a37a616efaaa84e
Kushant Sunkara <kushantsunkara@Kushant-Sunkaras-MacBook-Pro-3.local>
2019-04-28 17:24:06 -0400

    fix conflict, konstantinosTzogit add --all

--- KonstantinosTzo/code.py
+++ KonstantinosTzo/code.py
@@ -8,24 +8,6 @@ from math import sqrt
 
 
 def average(series):
-<<<<<<< HEAD:eda_hw.py
-    """
-    implements the average of a pandas series from scratch
-    suggested functions:
-    len(list)
-    sum(list)
-    you should get the same result as calling .mean() on your series
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.mean.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
-    """
-    x=len(series)
-    y=sum(series)
-    avg = y/x
-    return avg 
-    #pass
-#print (average([1,2,3,4,5,6,7,8]))
-=======
 
     y = len(series)
     x = sum(series)
@@ -33,8 +15,7 @@ def average(series):
     av = x/y
     return(av)
     
->>>>>>> upstream/master:KonstantinosTzo/code.py
-
+    
 def standard_deviation(series):
 
     r = 0
@@ -59,18 +40,7 @@ def standard_deviation(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html
     """
-<<<<<<< HEAD:eda_hw.py
-    a = average(series)
-    sum=0 
-    for i in range(len(series)):
-        sum += ((series[i]-a)**2)
-    return math.sqrt((sum)/(len(series)-1))
 
-#print(standard_deviation([1,3,8,11,15,17,22]))
-    #pass
-=======
-
->>>>>>> upstream/master:KonstantinosTzo/code.py
 
 def median(series):
 
@@ -93,18 +63,4 @@ def median(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.median.html
     https://pandas.pydata.org/pandas-docs/version/0.23.0/generated/pandas.Series.median.html
-<<<<<<< HEAD:eda_hw.py
     """
-    series=sorted(series)
-    if len(series)%2 == 0:
-        i=len(series)//2
-        me = (series[i-1]+series[i])/2
-    else:
-        i=len(series)//2 
-        me = series[i]
-    return me
-    
-#print(median([1,3,4,6,9,13,14,17]))
-=======
-    """
->>>>>>> upstream/master:KonstantinosTzo/code.py
